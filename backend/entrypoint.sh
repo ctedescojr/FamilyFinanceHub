817c38775c32faf3e6c703f7f3e1dd155e9b64fa
ctedescojr <ctedesco@usp.br>
2025-08-31 10:52:14 -0300

    backend dockerfile

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,6 +3,12 @@
 # Abort the script if any command fails
 set -e
 
+# Check if DJANGO_SETTINGS_MODULE is set
+if [ -z "$DJANGO_SETTINGS_MODULE" ]; then
+  echo "Error: DJANGO_SETTINGS_MODULE is not set."
+  exit 1
+fi
+
 echo "Waiting for the database..."
 python manage.py wait_for_db
 
